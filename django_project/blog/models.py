74b240d605665ba0c668acfa4c485c44a8851f0e
bbamsu99 <skqlgyrhk89@naver.com>
2023-03-14 17:51:44 +0900

    created apps and post model

---
+++ django_project/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+    #override 한 것이다
+
+    #누가 썼는지는 유저와 연동필요
